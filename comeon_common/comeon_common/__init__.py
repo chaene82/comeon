8b0b7a12b59b3020a3c5884711d4de9335a7e1b0
Christoph Haene <christoph.haene@gmail.com>
2017-11-01 10:56:20 +0100

    add other packages

--- comeon_common/comeon_common/__init__.py
+++ comeon_common/comeon_common/__init__.py
@@ -1,3 +1,5 @@
 #
+from .betbtc import getBetBtcEventData, getBetBtcMaketOdds
+from .Pinnacle import getPinnacleEventData, getPinnacleEventOdds
 from .getEvents import getEvents
 from .getOdds import getOdds
