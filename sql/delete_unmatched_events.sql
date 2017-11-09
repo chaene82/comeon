6b390e835e55d2b5fb64445d3ad3667e51c6bcf5
Christoph Haene <christoph.haene@gmail.com>
2017-11-09 11:22:48 +0100

    fix delete unmatch events

---
+++ sql/delete_unmatched_events.sql
@@ -0,0 +1,14 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET search_path = public, pg_catalog;
+
+\connect tennis
+
+delete from tbl_events where betbtc_event_id is null or pinnacle_event_id is null
