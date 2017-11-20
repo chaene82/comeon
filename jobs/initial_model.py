dde87d6537b835b2a575f149790a52fd7b52d527
Christoph Haene <christoph.haene@gmail.com>
2017-11-20 08:26:14 +0100

    add all models to the swisstennis script

--- jobs/initial_model.py
+++ jobs/initial_model.py
@@ -8,5 +8,6 @@ Created on Mon Nov 13 17:25:55 2017
 
 from comeon_etl import modelSwisstennis
 
-
+modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=4, ModelName='SW1MALL', StartDate="01/01/01")
+modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=12, ModelName='SW3MALL', StartDate="01/01/01")
 modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=52, ModelName='SW1YALL', StartDate="01/01/01")
