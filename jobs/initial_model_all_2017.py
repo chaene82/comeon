940a95829380b622a6f374db61806915f4a0262d
chrhae82 <christoph.haene@gmail.com>
2017-11-27 12:33:13 +0100

    create rating

--- jobs/initial_model_all_2017.py
+++ jobs/initial_model_all_2017.py
@@ -9,14 +9,14 @@ Created on Mon Nov 13 17:25:55 2017
 from comeon_etl import modelSwisstennis
 from comeon_etl import createRanking
 
-modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=52, ModelName='SW1YH', StartDate="01/02/17")
-modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=52, ModelName='SW1YG', StartDate="01/02/17")
-modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=52, ModelName='SW1YC', StartDate="01/02/17")
+#modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=52, ModelName='SW1YH', StartDate="01/02/17")
+#modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=52, ModelName='SW1YG', StartDate="01/02/17")
+#modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=52, ModelName='SW1YC', StartDate="01/02/17")
 
 
-modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=4, ModelName='SW1MALL', StartDate="01/02/17")
-modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=52, ModelName='SW1YALL', StartDate="01/02/17")
-modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=12, ModelName='SW3MALL', StartDate="01/02/17")
+#modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=4, ModelName='SW1MALL', StartDate="01/02/17")
+#modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=52, ModelName='SW1YALL', StartDate="01/02/17")
+#modelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=12, ModelName='SW3MALL', StartDate="01/02/17")
 
 
 createRanking()
