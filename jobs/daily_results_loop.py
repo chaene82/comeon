c65f992354e9f65cf118ad35b807e2702b3a2bc1
Christoph Haene <christoph.haene@gmail.com>
2018-02-08 15:02:15 +0100

    go live support

--- jobs/daily_results_loop.py
+++ jobs/daily_results_loop.py
@@ -11,6 +11,10 @@ import os
 from comeon_etl import  etl_te_get_matches, etl_import_te_daily_results, etl_transform_te_results, etl_import_te_daily_player, etl_te_get_missing_players
 from comeon_etl import etl_transform_te_players, etl_transform_te_matchdetails, etl_import_te_daily_matchdetails
 from comeon_etl import calcFairOdds, etl_te_get_matchesdetails, weeklyModelSwisstennis, etl_te_get_ranking, etl_import_te_weekly_ranking, createRanking
+from comeon_common import startBetLogging
+
+log = startBetLogging("etl")
+
 
 #sqllite3_path = 'c:\\users/haenec/python/home/comeon/data/te_data.db'
 
@@ -18,6 +22,8 @@ todate = datetime.now().date()- timedelta(days=10000)
 date = datetime.strptime("01/01/10", "%d/%m/%y").date()
 
 while date <= todate :
+    
+    log.info("load data for " + str(date))
     
     try:
         os.remove('te_data.db')
@@ -39,6 +45,8 @@ while date <= todate :
     calcFairOdds()
     
     if date.weekday() == 0 :        
+        log.info("load weekly ranking for " + str(date))
+        
         etl_te_get_ranking(date)
         etl_import_te_weekly_ranking()
         weeklyModelSwisstennis(start_value=0.1, days_between_run=7, look_back_weeks=52, ModelName='SW1YH', date=date)
